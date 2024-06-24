e430f21320ab771e6f6c57654746066dfb6e99d4
Moment <73689580+xun082@users.noreply.github.com>
2024-06-24 11:53:01 +0800

    style: 🎨 beautify the help page display (#9)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Table from 'cli-table3';
 
 import packageJson from '../package.json' assert { type: 'json' };
 
+import { colorize } from './utils';
 import { setConfig, getConfig, initializeProject } from './core/config';
 import { ConfigItem } from './types';
 import createComponents from './core/components';
@@ -16,13 +17,23 @@ async function main() {
   try {
     await initializeProject();
   } catch (error) {
-    console.error('Error ensuring config file exists:', error);
+    console.error(colorize('Error ensuring config file exists:', 'red'), error);
     process.exit(1); // 如果无法确保配置文件存在，则退出进程
   }
 
   const program = new Command();
 
-  program.version(packageJson.version).description('Moment!!!');
+  program
+    .version(packageJson.version)
+    .description(
+      colorize(
+        `南有嘉鱼，烝然罩罩。君子有酒，嘉宾式燕以乐。\n` +
+          `南有嘉鱼，烝然汕汕。君子有酒，嘉宾式燕以衎。\n` +
+          `南有樛木，甘瓠累之。君子有酒，嘉宾式燕绥之。\n` +
+          `南有樛木，甘瓠蕡蕡。君子有酒，嘉宾式燕以仁。`,
+        'green',
+      ),
+    );
 
   program
     .command('commit')
@@ -48,9 +59,9 @@ async function main() {
     .action(async (key, value) => {
       try {
         await setConfig(key as keyof ConfigItem, value);
-        console.log(`Config set: ${key} = ${value}`);
+        console.log(colorize(`Config set: ${key} = ${value}`, 'green'));
       } catch (error) {
-        console.error(`Error setting config: ${error}`);
+        console.error(colorize(`Error setting config: ${error}`, 'red'));
       }
     });
 
@@ -60,9 +71,9 @@ async function main() {
     .action(async (key) => {
       try {
         const value = await getConfig(key as keyof ConfigItem);
-        console.log(`${key} = ${value}`);
+        console.log(colorize(`${key} = ${value}`, 'cyan'));
       } catch (error) {
-        console.error(`Error getting config: ${error}`);
+        console.error(colorize(`Error getting config: ${error}`, 'red'));
       }
     });
 
@@ -77,7 +88,7 @@ async function main() {
     .command('hooks <name>')
     .description('Add a new Hooks')
     .action(async (name) => {
-      console.log(name);
+      console.log(colorize(name, 'yellow'));
       generatorHooks(name);
     });
 
@@ -85,7 +96,7 @@ async function main() {
   program.addHelpText('after', () => {
     // 创建表格
     const table = new Table({
-      head: ['Command', 'Description'],
+      head: [colorize('Command', 'green'), colorize('Description', 'green')],
       colWidths: [30, 70],
       style: {
         head: ['cyan'],
@@ -96,28 +107,28 @@ async function main() {
     // 填充表格内容
     table.push(
       [
-        'commit',
-        'Generate a commit message\nAI will automatically generate submission information for you.\n\nExample:\n  $ ai commit',
+        colorize('commit', 'cyan'),
+        `Generate a commit message\nAI will automatically generate submission information for you.\n\nExample:\n  ${colorize('$ ai commit', 'blue')}`,
       ],
       [
-        'review',
+        colorize('review', 'cyan'),
         'Generate a code review\nAI will automatically generate code review information for you.\n\nExample:\n  $ ai review',
       ],
       [
-        'set <key> <value>',
-        'Set a global configuration key and value.\n\nParameters:\n  key: The configuration key\n  value: The value to set\n\nExample:\n  $ ai set username john_doe',
+        colorize('set <key> <value>', 'cyan'),
+        `Set a global configuration key and value.\n\nParameters:\n  key: The configuration key\n  value: The value to set\n\nExample:\n  ${colorize('$ ai set username john_doe', 'blue')}`,
       ],
       [
-        'get <key>',
-        'Get a global configuration value.\n\nParameters:\n  key: The configuration key to retrieve\n\nExample:\n  $ ai get username',
+        colorize('get <key>', 'cyan'),
+        `Get a global configuration value.\n\nParameters:\n  key: The configuration key to retrieve\n\nExample:\n  ${colorize('$ ai get username', 'blue')}`,
       ],
       [
-        'component <name> [path]',
-        'Add a new component.\n\nParameters:\n  name: The name of the component\n  path: (Optional) The path to add the component\n\nExample:\n  $ ai component Button src/components',
+        colorize('component <name> [path]', 'cyan'),
+        `Add a new component.\n\nParameters:\n  name: The name of the component\n  path: (Optional) The path to add the component\n\nExample:\n  ${colorize('$ ai component Button src/components', 'blue')}`,
       ],
       [
-        'hooks <name>',
-        'Add a new Hooks.\n\nParameters:\n  name: The name of the hooks\n\nExample:\n  $ ai hooks useCustomHook',
+        colorize('hooks <name>', 'cyan'),
+        `Add a new Hooks.\n\nParameters:\n  name: The name of the hooks\n\nExample:\n  ${colorize('$ ai hooks useCustomHook', 'blue')}`,
       ],
     );
 
