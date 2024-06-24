ae63612539665965af9fe296c8ff345cd11b0562
Moment <73689580+xun082@users.noreply.github.com>
2024-06-24 17:27:38 +0800

    feat: 🚀 automatically delete the artifacts from the previous build each time during build, and remove sourcemaps in production environment. (#12)

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
+import del from 'rollup-plugin-delete';
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'src/index.ts',
@@ -10,9 +13,16 @@ export default {
     {
       file: 'dist/index.js',
       format: 'esm',
-      sourcemap: true,
+      sourcemap: !production,
     },
   ],
-  plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), json(), terser()],
+  plugins: [
+    production && del({ targets: 'dist/*' }),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    json(),
+    terser(),
+  ],
   external: [],
 };
