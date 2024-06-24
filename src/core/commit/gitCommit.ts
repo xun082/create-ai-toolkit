458bb362e3920984b982f51ed48425c07603ad31
Eadweard <aurora.coder.zqf@gmail.com>
2024-06-24 14:55:21 +0800

    ci: test manual input

--- src/core/commit/gitCommit.ts
+++ src/core/commit/gitCommit.ts
@@ -2,6 +2,15 @@ import { cancel, confirm, outro, text } from '@clack/prompts';
 
 import { autoCommit } from '@/core/commit/help';
 
+async function selectAIMsgOrManualMsg(msg: string) {
+  const code = await autoCommit(msg);
+  if (code === 0) {
+    outro('提交成功');
+  } else {
+    cancel('提交失败');
+  }
+}
+
 export default async (aiCommitMsg: string) => {
   const answer = await confirm({
     message: '是否使用以上提交信息？',
@@ -9,15 +18,12 @@ export default async (aiCommitMsg: string) => {
     inactive: '取消',
   });
   if (answer) {
-    const code = await autoCommit(aiCommitMsg);
-    if (code === 0) {
-      outro('提交成功');
-    } else {
-      cancel('提交失败');
-    }
+    await selectAIMsgOrManualMsg(aiCommitMsg);
   } else {
     // 让用户自己手动输入提交信息再提交
-    const msg = await text({ message: 'commit信息', placeholder: '请输入commit信息' });
-    console.log(msg);
+    const manualMsg = await text({ message: 'commit信息', placeholder: '请输入commit信息' });
+    if (typeof manualMsg === 'string' && manualMsg) {
+      await selectAIMsgOrManualMsg(manualMsg as string);
+    }
   }
 };
